d8ff40d2ac6aabe07b16386b648c865f7a1ee731
costa.alessandro <costa.alessandro@ifood.com.br>
2023-05-31 14:12:14 -0300

    Feat: Initial commit

---
+++ src/main/java/br/com/abcosta/FoodcardApplication.java
@@ -0,0 +1,13 @@
+package br.com.abcosta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodcardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodcardApplication.class, args);
+	}
+
+}

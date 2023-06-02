939d2b154608ae5e022e0a92b6c9941f58f516c8
costa.alessandro <costa.alessandro@ifood.com.br>
2023-06-02 12:13:49 -0300

    Feat: Rename application

--- src/main/java/br/com/abcosta/FoodCardApplication.java
+++ src/main/java/br/com/abcosta/FoodCardApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FoodcardApplication {
+public class FoodCardApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FoodcardApplication.class, args);
+		SpringApplication.run(FoodCardApplication.class, args);
 	}
 
 }

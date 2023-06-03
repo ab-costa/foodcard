1cba4e5a1cbf3c8973d960cd5c877ae5effdc540
costa.alessandro <costa.alessandro@ifood.com.br>
2023-06-03 16:46:50 -0300

    Fix: Changing card to cart

--- src/main/java/br/com/abcosta/coreapi/commands.kt
+++ src/main/java/br/com/abcosta/coreapi/commands.kt
@@ -4,26 +4,26 @@ import org.axonframework.commandhandling.RoutingKey
 import org.axonframework.modelling.command.TargetAggregateIdentifier
 import java.util.UUID
 
-class CreateFoodCardCommand(
+class CreateFoodCartCommand(
     @RoutingKey
-    val foodCardId: UUID
+    val foodCartId: UUID
 )
 
 data class SelectProductCommand(
     @TargetAggregateIdentifier
-    val foodCardId: UUID,
+    val foodCartId: UUID,
     val productId: UUID,
     val quantity: Int
 )
 
 data class DeselectProductCommand(
     @TargetAggregateIdentifier
-    val foodCardId: UUID,
+    val foodCartId: UUID,
     val productId: UUID,
     val quantity: Int
 )
 
 data class ConfirmOrderCommand(
     @TargetAggregateIdentifier
-    val foodCardId: UUID
+    val foodCartId: UUID
 )
--- src/main/java/br/com/abcosta/coreapi/events.kt
+++ src/main/java/br/com/abcosta/coreapi/events.kt
@@ -2,22 +2,22 @@ package br.com.abcosta.coreapi
 
 import java.util.UUID
 
-class FoodCardCreatedEvent(
-    val foodCardId: UUID
+class FoodCartCreatedEvent(
+    val foodCartId: UUID
 )
 
 data class ProductSelectedEvent(
-    val foodCardId: UUID,
+    val foodCartId: UUID,
     val productId: UUID,
     val quantity: Int
 )
 
 data class ProductDeselectedEvent(
-    val foodCardId: UUID,
+    val foodCartId: UUID,
     val productId: UUID,
     val quantity: Int
 )
 
 data class OrderConfirmedEvent(
-    val foodCardId: UUID
+    val foodCartId: UUID
 )
